6210cb4f66064bfd122e68a1ded8698b51b03b68
alex parfenov <alexp-2004@mail.ru>
2022-05-24 20:53:39 +0700

    finished 5 etap

---
+++ RGR/Models/StaticTabs/BaseballPlayerTab.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RGR.Models.Database;
+
+
+namespace RGR.Models
+{
+    public class BaseballPlayerTab : StaticTab
+    {
+        public BaseballPlayerTab(string h = "", DbSet<BaseballPlayer>? dBS = null) : base(h)
+        {
+            DBS = dBS;
+            DataColumns = new List<string>();
+            DataColumns.Add("ProperName");
+            DataColumns.Add("TeamSName");
+            DataColumns.Add("Born");
+            DataColumns.Add("Age");
+            DataColumns.Add("Bats");
+        }
+        new public DbSet<BaseballPlayer>? DBS { get; set; }
+    }
+}
